c41cf2ad8addc1754ba0a88bedd28f63e443fc04
Shadrachp <shadrachgabriel@icloud.com>
2020-10-16 21:25:33 +0800

    Add cloudinary to home

--- src/assets/javascript/components/home/Home.jsx
+++ src/assets/javascript/components/home/Home.jsx
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
 import "../../../stylesheets/home.css";
+import {Image, Transformation, CloudinaryContext} from 'cloudinary-react';
+
 
 class Home extends Component {
   constructor(props) {
     super( props );
-
-    this.state = ({
-      title: props.title,
-    });
+    console.log(this.props);
   }
 
   render() {
     return(
-      <div className="home-container">
-        <div></div>
-        <div className="feed">
-          <div className="wrapper">
-            <div className="content">
-                <img src="https://img-9gag-fun.9cache.com/photo/aqnprzv_460s.jpg"/>
+      <CloudinaryContext
+        cloudName={this.props.cloudName}
+        uploadPreset={this.props.uploadPreset}
+      >
+        <div className="home-container">
+          <div></div>
+          <div className="feed">
+            <div className="wrapper">
+              <div className="content">
+                <Image
+                  publicId="https://res.cloudinary.com/dk19dzivc/image/upload/v1602716213/samples/ecommerce/accessories-bag.jpg"
+                />
+              </div>
+              <div className="content">
+                <Image
+                  publicId="https://res.cloudinary.com/dk19dzivc/image/upload/v1602853429/grameme/paa1wgdwiolpmhxi9fsz.jpg"
+                />
+              </div>
+              <div className="content">
+                <Image
+                  publicId="https://res.cloudinary.com/dk19dzivc/image/upload/v1602853493/grameme/rczub12eexn6apto74xb.jpg"
+                />
+              </div>
+              <div className="content">
+                <Image
+                  publicId="https://res.cloudinary.com/dk19dzivc/image/upload/v1602853785/grameme/lqbcjdqsdwxmzjt0rnan.jpg"
+                />
+              </div>
             </div>
-            <div className="content">
-                <img src="https://img-9gag-fun.9cache.com/photo/aqnprzv_460s.jpg"/>
-            </div>
-
           </div>
+          <div></div>
         </div>
-        <div></div>
-      </div>
+      </CloudinaryContext>
     );
   }
 }
