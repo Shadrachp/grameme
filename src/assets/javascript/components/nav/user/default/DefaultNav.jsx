63b7952a50909503448ab7472eb36e7f5f499086
Shadrachp <shadrachgabriel@icloud.com>
2020-11-15 13:45:05 +0800

    Add style for cursor

--- src/assets/javascript/components/nav/user/default/DefaultNav.jsx
+++ src/assets/javascript/components/nav/user/default/DefaultNav.jsx
@@ -6,10 +6,14 @@ const DefaultNav = (props) => {
 
   return (
     <>
-      <li onClick={() => props.show()} id="log-in">
+      <li onClick={() => props.show()}
+          id="log-in"
+          className="btn">
         <Button  text="Log In"/>
       </li>
-      <li onClick={() => props.show()} id="sign-up">
+      <li onClick={() => props.show()}
+          id="sign-up"
+          className="btn">
         <Button  text="Sign Up"/>
       </li>
 
