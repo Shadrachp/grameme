d8de57a48eab93058fc23ac5a3755ce4be4b1726
Shadrachp <shadrachgabriel@icloud.com>
2020-11-17 04:07:58 +0800

    Add user and password input html css

--- src/assets/javascript/components/modal/FormModal.jsx
+++ src/assets/javascript/components/modal/FormModal.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../../../assets/stylesheets/modal.scss";
 
 const FormModal = (props) => {
+  const [clearPlaceholder, setClearPlaceholder] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+
+  const userForm = () => {
+    if(username.length === 0)
+      return false;
+    return true
+  };
+
+  const handleChange = (e) => {
+    setUsername = e.target.value;
+    console.log(username);
+  };
 
   return (
 
@@ -15,7 +29,23 @@ const FormModal = (props) => {
           </div>
         </div>
         <div className="modal-body">
-            <div>test2</div>
+            <div className="modal-body-tabs">
+
+            </div>
+            <div className="modal-body-input">
+              <div className="modal-input-email">
+                <label>U S E R N A M E</label>
+                <input type="email" />
+              </div>
+              <div className="modal-input-email">
+                <label>P A S S W O R D</label>
+                <input type="password"/>
+              </div>
+            </div>
+            <div className="modal-body-extras">
+              <div>test3</div>
+              <div>test3</div>
+            </div>
         </div>
         <div className="modal-footer">
             <div>test3</div>
